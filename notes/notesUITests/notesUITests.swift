da59f353ec131267fd72e340707cd849cb73dace
Rubén <ruben.sr.apple@gmail.com>
2024-07-04 18:29:31 +0200

    Add UI Test

--- notes/notesUITests/notesUITests.swift
+++ notes/notesUITests/notesUITests.swift
@@ -9,33 +9,24 @@ import XCTest
 
 final class notesUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testCreateNote() throws {
+        
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.toolbars["Toolbar"]/*@START_MENU_TOKEN@*/.staticTexts["Crear nota"]/*[[".otherElements[\"Crear nota\"]",".buttons[\"Crear nota\"].staticTexts[\"Crear nota\"]",".staticTexts[\"Crear nota\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 0).children(matching: .other).element(boundBy: 1).children(matching: .other).element.tap()
+        
+        let collectionViewsQuery2 = collectionViewsQuery
+        collectionViewsQuery2/*@START_MENU_TOKEN@*/.textViews["Título"]/*[[".cells.textViews[\"Título\"]",".textViews[\"Título\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.textViews["Título"]/*[[".cells.textViews[\"Título\"]",".textViews[\"Título\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("Test UI")
+        
+        let textoTextView = collectionViewsQuery2/*@START_MENU_TOKEN@*/.textViews["Texto"]/*[[".cells.textViews[\"Texto\"]",".textViews[\"Texto\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        textoTextView.tap()
+        collectionViewsQuery2/*@START_MENU_TOKEN@*/.textViews["Texto"]/*[[".cells.textViews[\"Texto\"]",".textViews[\"Texto\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("Texto del test UI")
+        app.navigationBars["Nueva nota"]/*@START_MENU_TOKEN@*/.buttons["Crear nota"]/*[[".otherElements[\"Crear nota\"].buttons[\"Crear nota\"]",".buttons[\"Crear nota\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+                
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
